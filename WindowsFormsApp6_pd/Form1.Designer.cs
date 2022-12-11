9100dd15d58b4538060b2a2b4f561ba5b95fa373
denisfilonov <dean.mitz6@gmail.com>
2022-12-11 20:58:30 +0100

    Add project files.

---
+++ WindowsFormsApp6_pd/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp6_pd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chbFirst = new System.Windows.Forms.CheckBox();
+            this.chbSecond = new System.Windows.Forms.CheckBox();
+            this.chbThird = new System.Windows.Forms.CheckBox();
+            this.chbFourth = new System.Windows.Forms.CheckBox();
+            this.chbFifth = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // chbFirst
+            // 
+            this.chbFirst.AutoSize = true;
+            this.chbFirst.Location = new System.Drawing.Point(186, 85);
+            this.chbFirst.Name = "chbFirst";
+            this.chbFirst.Size = new System.Drawing.Size(36, 20);
+            this.chbFirst.TabIndex = 0;
+            this.chbFirst.Text = "1";
+            this.chbFirst.UseVisualStyleBackColor = true;
+            this.chbFirst.CheckedChanged += new System.EventHandler(this.Check);
+            // 
+            // chbSecond
+            // 
+            this.chbSecond.AutoSize = true;
+            this.chbSecond.Location = new System.Drawing.Point(186, 111);
+            this.chbSecond.Name = "chbSecond";
+            this.chbSecond.Size = new System.Drawing.Size(36, 20);
+            this.chbSecond.TabIndex = 1;
+            this.chbSecond.Text = "2";
+            this.chbSecond.UseVisualStyleBackColor = true;
+            this.chbSecond.CheckedChanged += new System.EventHandler(this.Check);
+            // 
+            // chbThird
+            // 
+            this.chbThird.AutoSize = true;
+            this.chbThird.Location = new System.Drawing.Point(186, 137);
+            this.chbThird.Name = "chbThird";
+            this.chbThird.Size = new System.Drawing.Size(36, 20);
+            this.chbThird.TabIndex = 2;
+            this.chbThird.Text = "3";
+            this.chbThird.UseVisualStyleBackColor = true;
+            this.chbThird.CheckedChanged += new System.EventHandler(this.Check);
+            // 
+            // chbFourth
+            // 
+            this.chbFourth.AutoSize = true;
+            this.chbFourth.Location = new System.Drawing.Point(186, 163);
+            this.chbFourth.Name = "chbFourth";
+            this.chbFourth.Size = new System.Drawing.Size(36, 20);
+            this.chbFourth.TabIndex = 3;
+            this.chbFourth.Text = "4";
+            this.chbFourth.UseVisualStyleBackColor = true;
+            this.chbFourth.CheckedChanged += new System.EventHandler(this.Check);
+            // 
+            // chbFifth
+            // 
+            this.chbFifth.AutoSize = true;
+            this.chbFifth.Location = new System.Drawing.Point(186, 189);
+            this.chbFifth.Name = "chbFifth";
+            this.chbFifth.Size = new System.Drawing.Size(36, 20);
+            this.chbFifth.TabIndex = 4;
+            this.chbFifth.Text = "5";
+            this.chbFifth.UseVisualStyleBackColor = true;
+            this.chbFifth.CheckedChanged += new System.EventHandler(this.Check);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 331);
+            this.Controls.Add(this.chbFifth);
+            this.Controls.Add(this.chbFourth);
+            this.Controls.Add(this.chbThird);
+            this.Controls.Add(this.chbSecond);
+            this.Controls.Add(this.chbFirst);
+            this.Name = "Form1";
+            this.Text = "Отметь все галочки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chbFirst;
+        private System.Windows.Forms.CheckBox chbSecond;
+        private System.Windows.Forms.CheckBox chbThird;
+        private System.Windows.Forms.CheckBox chbFourth;
+        private System.Windows.Forms.CheckBox chbFifth;
+    }
+}
+
---
+++ WindowsFormsApp6_pd/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6_pd
+{
+    public partial class Form1 : Form
+    {
+        private List<CheckBox> _checkBoxes;
+        public Form1()
+        {
+            InitializeComponent();
+            _checkBoxes = new List<CheckBox>();
+            _checkBoxes.Add(chbFirst);
+            _checkBoxes.Add(chbSecond);
+            _checkBoxes.Add(chbThird);
+            _checkBoxes.Add(chbFourth);
+            _checkBoxes.Add(chbFifth);
+        }
+
+        private void Check(object sender, EventArgs e)
+        {
+            if (sender is CheckBox checkBox) // <==>  var checkBox = sender as CheckBox; without if() {}
+            {
+                int index = _checkBoxes.IndexOf(checkBox);
+
+                for (int i = 0; i < 2; i++)
+                {
+                    if (index + 1 >= _checkBoxes.Count)
+                    {
+                        index = 0;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+
+                    _checkBoxes[index].CheckedChanged -= Check;
+                    _checkBoxes[index].Checked = !_checkBoxes[index].Checked;
+                    _checkBoxes[index].CheckedChanged += Check;
+                    for (int j = 0; j < _checkBoxes.Count; j++)
+                    {
+                        if (_checkBoxes[j].Checked)
+                        {
+                            _checkBoxes[j].Enabled = false;
+                        }
+                        else
+                        {
+                            _checkBoxes[j].Enabled = true;
+                        }
+                    }
+                }
+
+                int counter = 0;
+                for (int i = 0; i < _checkBoxes.Count; i++)
+                {
+                    if (_checkBoxes[i].Checked)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 5)
+                {
+                    MessageBox.Show("Вы победили!", "Поздравляю", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        /*
+        private void chbFirst_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chbFirst.Checked)
+            {
+                chbFirst.Checked = true;
+                chbFirst.Enabled = false;
+
+                //if (!chbSecond.Checked)
+                //{
+                //    chbSecond.Checked = true;
+                //    chbSecond.Enabled = false;
+                //}
+                //else
+                //{
+                //    chbSecond.Checked = false;
+                //    chbSecond.Enabled = true;
+                //}
+
+                //if (!chbThird.Checked)
+                //{
+                //    chbThird.Checked = true;
+                //    chbThird.Enabled = false;
+                //}
+                //else
+                //{
+                //    chbThird.Checked = false;
+                //    chbThird.Enabled = true;
+                //}
+            }
+        }
+        */
+    }
+}
